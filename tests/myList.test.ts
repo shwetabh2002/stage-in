5e799b0000d3bcf8f1a7fb2c8db2fc3c90f6a9dd
manish gupta <shwetabhgupta2002@gmail.com>
2024-06-30 10:45:44 +0530

    j

--- tests/myList.test.ts
+++ tests/myList.test.ts
@@ -12,6 +12,7 @@ describe('Wishlist API', () => {
 
   afterAll(async () => {
     await mongoose.connection.close();
+      process.exit(0); 
   });
 
   it('should add an item to the wishlist using curl equivalent', async () => {
